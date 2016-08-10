eed2892749179c19773c20cf6f5c95dc677621d9
JDarkGreen <jgomez.4net@gmail.com>
2016-08-10 18:17:37 -0500

    actualizacion mujer

--- themes/custom_theme/taxonomy-producto_category.php
+++ themes/custom_theme/taxonomy-producto_category.php
@@ -82,11 +82,87 @@ endif;
 						),
 					); 
 
-					$all_products = get_posts( $args );
+					$the_query = new WP_Query( $args ); #var_dump($all_products);
 
-					var_dump($all_products);
-
+					#Ejecutar el loop del query
+					if( $the_query->have_posts() ) :
+					while( $the_query->have_posts() ) : $the_query->the_post();
 				?>
+					
+					<!-- Artículo preview de Categoría de Producto -->
+					<article class="productoItemPreview">
+						<div class="row">
+
+							<!-- Imagen -->
+							<div class="col-xs-12 col-sm-5">
+								
+								<figure class="borderPurpleShadow borderPurpleDarkShadow--big">
+									<a href="<?= get_the_permalink(); ?>">
+									<?php  
+										if( has_post_thumbnail() ) :
+											echo get_the_post_thumbnail( get_the_ID() , 'full' , array('class'=>'img-fluid') );
+										endif;
+									?>
+									</a>
+								</figure> <!-- /.end figure -->
+
+							</div> <!-- /.col-xs-12 col-sm-4 -->
+
+							<!-- Texto -->
+							<div class="col-xs-12 col-sm-7">
+								
+								<div class="productoItemPreview__content">
+									
+									<!-- Titulo -->
+									<h3 class="text-uppercase"> <?= get_the_title(); ?></h3>
+
+									<!-- El Código -->
+									<h4 class="text-xs-uppercase"> COD 046 </h4>
+
+									<!-- El extracto -->
+									<?php  
+										$limit_words = 15;
+
+										$the_excerpt = strip_tags( get_the_excerpt() );
+										$the_excerpt = wp_trim_words( $the_excerpt , $limit_words , "..." );
+
+										$the_content = strip_tags( get_the_content() );
+										$the_content = wp_trim_words( $the_content , $limit_words , "..." );
+
+										if( !empty($the_excerpt) ) :
+											echo apply_filters( "the_content", $the_excerpt );
+										else:
+											echo apply_filters( "the_content", $the_content );
+										endif;
+									?>
+
+									<!-- La separación --> <br><br>
+
+									<!-- Botón de ver más -->
+									<a href="<?= get_the_permalink(); ?>" class="btnCommon__show-more text-uppercase">
+										<?= __( "ver más" , "LANG" ); ?> </a>
+
+									<!-- Sección Compartir -->
+									<?php 
+										#Setear id_compartir
+										$id_share   = get_the_ID();
+										#Setear link compartir 
+										$link_share = get_the_permalink();
+										#Setear link title 
+										$link_title = get_the_title();
+
+										#Plantilla 
+										include(locate_template("partials/share/shared-buttons.php") ); 
+									?>
+
+								</div> <!-- /.productoItemPreview__content -->
+
+							</div> <!-- /.col-xs-12 col-sm-8 -->
+							
+						</div> <!-- /.row -->
+					</article> <!-- /.productoItemPreview -->
+
+				<?php endwhile; endif; wp_reset_postdata(); ?>
 				
 			</div> <!-- col-xs-12 col-sm-8 -->
 			
