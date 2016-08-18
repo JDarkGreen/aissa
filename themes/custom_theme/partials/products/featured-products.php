2d1ec9f9115fa7bee3dd4486433e28762529c777
JDarkGreen <jgomez.4net@gmail.com>
2016-08-18 16:38:51 -0500

    actualizacion de home

--- themes/custom_theme/partials/products/featured-products.php
+++ themes/custom_theme/partials/products/featured-products.php
@@ -4,8 +4,6 @@
 * ESTA PLANTILLA INCLUYE EL PÁGINADOR DE PRODUCTOS DESTACADOS
 **/
 
-/* variable paged paginación */
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
 /* Elementos por página */
 $posts_per_page = 9;
@@ -13,7 +11,7 @@ $posts_per_page = 9;
 #1.- Obtener todos los productos destacados
 
 $args = array(
-	'posts_per_page' => $posts_per_page,
+	'posts_per_page' => -1,
 	'post_type'      => 'producto-theme',
 	'post_status'    => 'publish',
 	'meta_query'     => array(
@@ -26,13 +24,10 @@ $args = array(
 	'meta_key' => 'mb_sort_elements_select',
 	'orderby'  => 'meta_value_num',
 	'order'    => 'ASC',
-	'paged'    => $paged,
 );
 
 #EL QUERY 
 $the_query = new WP_Query( $args );
-
-
 
 #Hacer recorrido
 if( $the_query->have_posts() ) :
@@ -49,62 +44,126 @@ if( $the_query->have_posts() ) :
 	#Post mostrados 
 	$number_show_posts = $the_query->post_count;
 
-	while( $the_query->have_posts() ): $the_query->the_post();	
+	#Si el numero de post es mayor a los post por pagina entonces hacer galeria
+	if( $number_show_posts > $posts_per_page ) :
 ?>
 
-	<!-- ABRIR FILA -->
-	<?php if( $control_row % $item_per_row === 0  ) : ?><div class="row"><?php endif; ?>
+		<!-- Abrir Carousel De Productos -->
+		<div id="carousel-productos-theme" class="js-carousel-gallery" data-items="1" data-items-responsive="1" data-margins="5" data-dots="true" data-autoplay="false">
+		
+			<?php 
+				#Variable de control de section
+				$control_section = 0;
 
-		<!-- Si tiene imagen destacada -->
-		<?php if( has_post_thumbnail() ) : ?>
-	
-			<!-- Item de Producto -->
-			<div class="col-xs-12 col-sm-4">
+				#Hacemos recorrido para mostrar productos
+				while( $the_query->have_posts() ): $the_query->the_post();
+			?>
+				<!-- Abrir SECCIÓN -->
+				<?php if( $control_section % $posts_per_page === 0  ) : ?>
+					<div class="sectionProducto__container">
+				<?php endif; ?>
+
+				<!-- Si tiene imagen destacada -->
+				<?php if( has_post_thumbnail() ) : ?>
+			
+					<!-- Item de Producto -->
+					<div class="col-xs-12 col-sm-4">
+						
+						<article class="itemProducto__preview">
+							<!-- Imagen Destacada -->
+							<figure class="borderPurpleShadow borderPurpleDarkShadow--big">
+								<a href="<?= get_permalink(); ?>">
+								<?= get_the_post_thumbnail( get_the_ID() , 'full' , array('class'=>'img-fluid') ); ?>
+								</a>
+							</figure> <!-- /. -->
+
+							<!-- Nombre de Producto -->
+							<h3 class="text-xs-center colorPurpleDark"> <?= get_the_title(); ?></h3>
+
+							<!-- Producto ver más -->
+							<div class="text-xs-center">
+								<a href="<?= get_permalink(); ?>" class="btnCommon__show-more text-uppercase"> 
+									<?php _e( "ver más" , LANG  );  ?>
+								</a> <!-- / -->
+							</div> <!-- /.text-xs-center -->
+
+						</article> <!-- /.itemProducto__preview -->
+
+					</div> <!-- /.col-xs-4 -->
+			
+				<?php endif; #Fin condicional si tiene imagen  ?>
+
+				<!-- Cerrar SECCIÓN -->
+				<?php if( $control_section + 1 === $number_show_posts or ($control_section !== 0 and ($control_section + 1) % $posts_per_page === 0)  ):  ?>
+						</div> <!-- /fin de sección -->
+				<?php endif; ?>
+
+			<?php $control_section++; endwhile; ?>
+
+		</div> <!-- #fin de carousel de producto -->
+
+		<!-- Contenedor de Sección de Paginación -->
+		<section class="pageCommonPagination text-xs-right">
+			<?php  
+			#Hacer recorrido para setear dots
+			$pages = ceil( $number_show_posts / $posts_per_page );
 				
-				<article class="itemProducto__preview">
-					<!-- Imagen Destacada -->
-					<figure class="borderPurpleShadow borderPurpleDarkShadow--big">
-						<a href="<?= get_permalink(); ?>">
-						<?= get_the_post_thumbnail( get_the_ID() , 'full' , array('class'=>'img-fluid') ); ?>
-						</a>
-					</figure> <!-- /. -->
+			for( $i = 0 ; $i < $pages ; $i++ ) { ?>
+			
+			<a href="#" data-slider="carousel-productos-theme" data-to="<?= $i; ?>" class="gallery_indicator js-carousel-indicator"> <?= $i+1; ?> 
+			</a>
 
-					<!-- Nombre de Producto -->
-					<h3 class="text-xs-center colorPurpleDark"> <?= get_the_title(); ?></h3>
+			<?php } ?>
+		</section> <!-- /.pageCommonPagination -->
 
-					<!-- Producto ver más -->
-					<div class="text-xs-center">
-						<a href="<?= get_permalink(); ?>" class="btnCommon__show-more text-uppercase"> 
-							<?php _e( "ver más" , LANG  );  ?>
-						</a> <!-- / -->
-					</div> <!-- /.text-xs-center -->
+<?php 
+	#Si el numero de post es menor o igual a los post por pagina entonces solo mostrar
+	else: 
 
-				</article> <!-- /.itemProducto__preview -->
+		while( $the_query->have_posts() ): $the_query->the_post();	
+?>
+		<!-- ABRIR FILA -->
+		<?php if( $control_row % $item_per_row === 0  ) : ?><div class="row"><?php endif; ?>
 
-			</div> <!-- /.col-xs-4 -->
-	
-		<?php endif; #Fin condicional si tiene imagen  ?>
+			<!-- Si tiene imagen destacada -->
+			<?php if( has_post_thumbnail() ) : ?>
+		
+				<!-- Item de Producto -->
+				<div class="col-xs-12 col-sm-4">
+						
+					<article class="itemProducto__preview">
+						<!-- Imagen Destacada -->
+						<figure class="borderPurpleShadow borderPurpleDarkShadow--big">
+							<a href="<?= get_permalink(); ?>">
+							<?= get_the_post_thumbnail( get_the_ID() , 'full' , array('class'=>'img-fluid') ); ?>
+							</a>
+						</figure> <!-- /. -->
 
-	<!-- CERRAR FILA -->
-	<?php if( ($control_row == $number_show_posts - 1 ) || ($number_show_posts <= $item_per_row ) || ( $control_row == $min_num_per_row ) || ($control_row >= $max_num_per_row && ( $control_row - $max_num_per_row ) % $item_per_row == 0  ) ) : ?> 
-	</div><!-- /end row --> <?php endif; ?>
+						<!-- Nombre de Producto -->
+						<h3 class="text-xs-center colorPurpleDark"> <?= get_the_title(); ?></h3>
 
-<!--  -->
-<?php $control_row++; endwhile; ?>
+						<!-- Producto ver más -->
+						<div class="text-xs-center">
+							<a href="<?= get_permalink(); ?>" class="btnCommon__show-more text-uppercase"> 
+								<?php _e( "ver más" , LANG  );  ?>
+							</a> <!-- / -->
+						</div> <!-- /.text-xs-center -->
+
+					</article> <!-- /.itemProducto__preview -->
+
+				</div> <!-- /.col-xs-4 -->
+		
+			<?php endif; #Fin condicional si tiene imagen  ?>
+
+		<!-- CERRAR FILA -->
+		<?php if( ($control_row == $number_show_posts - 1 ) || ($number_show_posts <= $item_per_row ) || ( $control_row == $min_num_per_row ) || ($control_row >= $max_num_per_row && ( $control_row - $max_num_per_row ) % $item_per_row == 0  ) ) : ?> 
+		</div><!-- /end row --> <?php endif; ?>
+
+	<?php $control_row++; endwhile; ?>
 
 	<!-- Para seguridad Limpiar Floats --> <div class="clearfix"></div>
 
-	<!-- Páginación aquí -->
-	<section class="sectionPagination text-xs-right">
-		<?php  
-			/*
-			Numero total de páginas. Is the result of $found_posts / $posts_per_page 
-			*/
-			$pages = $the_query->max_num_pages;
-			for ( $i = 1 ; $i <= $pages ; $i++ ) { ?>
-			<a class="<?= $i == $paged ? 'active current' : '' ?>" href="<?= get_pagenum_link( $i ); ?>"> <?= $i; ?> </a>
-		<?php } /* endfor */ ?>
-	</section> <!-- /.sectionPagination -->
+	<?php endif; ?> <!-- /fin de condicional de post mayores o menores -->
 
 
 <?php endif; wp_reset_postdata(); ?>
